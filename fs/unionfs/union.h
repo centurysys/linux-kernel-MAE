781b76530e74dd3cbd6f2e231cbc7916a83bef30
Erez Zadok <ezk@cs.sunysb.edu>
2007-09-29 01:36:57 -0400

    Unionfs: compile if debug is off
    Signed-of-by: John Johansen <jjohansen@suse.de>
    Signed-off-by: Erez Zadok <ezk@cs.sunysb.edu>

--- fs/unionfs/union.h
+++ fs/unionfs/union.h
@@ -560,6 +560,7 @@ extern void __show_inode_counts(const struct inode *inode,
 #define unionfs_check_inode(i)		do { } while(0)
 #define unionfs_check_dentry(d)		do { } while(0)
 #define unionfs_check_file(f)		do { } while(0)
+#define unionfs_check_nd(n)		do { } while(0)
 #define show_branch_counts(sb)		do { } while(0)
 #define show_inode_times(i)		do { } while(0)
 #define show_dinode_times(d)		do { } while(0)
