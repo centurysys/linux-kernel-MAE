0ec3c2f7447d01fe07b9b5a9de0f88c47d65a895
Erez Zadok <ezk@cs.sunysb.edu>
2007-11-02 19:56:49 -0400

    Unionfs/VFS: no need to export 2 symbols in security/security.c
    Signed-off-by: Erez Zadok <ezk@cs.sunysb.edu>

--- security/security.c
+++ security/security.c
@@ -553,7 +553,6 @@ int security_inode_permission(struct inode *inode, int mask)
 		return 0;
 	return security_ops->inode_permission(inode, mask);
 }
-EXPORT_SYMBOL(security_inode_permission);
 
 int security_inode_setattr(struct dentry *dentry, struct iattr *attr)
 {
@@ -691,7 +690,6 @@ int security_file_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	return security_ops->file_ioctl(file, cmd, arg);
 }
-EXPORT_SYMBOL(security_file_ioctl);
 
 static inline unsigned long mmap_prot(struct file *file, unsigned long prot)
 {
