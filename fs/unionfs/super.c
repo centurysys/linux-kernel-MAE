e06740b64b92b2d22aceaf30ad08f8706d2b864c
Erez Zadok <ezk@cs.sunysb.edu>
2010-11-02 18:47:42 -0400

    Unionfs: call truncate_inode_pages+end_writeback in ->evict_inode
    Signed-off-by: Erez Zadok <ezk@cs.sunysb.edu>

--- fs/unionfs/super.c
+++ fs/unionfs/super.c
@@ -834,6 +834,9 @@ static void unionfs_evict_inode(struct inode *inode)
 	struct list_head *pos, *n;
 	struct unionfs_dir_state *rdstate;
 
+	truncate_inode_pages(&inode->i_data, 0);
+	end_writeback(inode);
+
 	list_for_each_safe(pos, n, &UNIONFS_I(inode)->readdircache) {
 		rdstate = list_entry(pos, struct unionfs_dir_state, cache);
 		list_del(&rdstate->cache);
