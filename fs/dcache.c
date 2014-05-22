29b93a5fcf03a84b0dd6859adcaf347157c7f81a
Sedat Dilek <sedat.dilek@gmail.com>
2014-05-23 01:08:07 +0200

    vfs: dcache: Export d_ancestor to modules
    With OverlayFS v22 I get the following build error when
    CONFIG_OVERLAYFS_FS=m
    
    ERROR: "d_ancestor" [fs/overlayfs/overlayfs.ko] undefined!
    
    Fix this by adding the missing export.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>

--- fs/dcache.c
+++ fs/dcache.c
@@ -2559,6 +2559,7 @@ struct dentry *d_ancestor(struct dentry *p1, struct dentry *p2)
 	}
 	return NULL;
 }
+EXPORT_SYMBOL(d_ancestor);
 
 /*
  * This helper attempts to cope with remotely renamed directories
