8502ca9308790cfa29571dcfc839961741e9d8ef
Miklos Szeredi <mszeredi@suse.cz>
2014-06-03 11:35:10 +0200

    shmem: support RENAME_EXCHANGE
    This is really simple in tmpfs since the VFS already takes care of
    shuffling the dentries.  Just adjust nlink on parent directories and touch
    c & mtimes.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Cc: Hugh Dickins <hughd@google.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -1995,12 +1995,37 @@ static int shmem_rename(struct inode *old_dir, struct dentry *old_dentry, struct
 	return 0;
 }
 
+static int shmem_exchange(struct inode *old_dir, struct dentry *old_dentry, struct inode *new_dir, struct dentry *new_dentry)
+{
+	bool old_is_dir = S_ISDIR(old_dentry->d_inode->i_mode);
+	bool new_is_dir = S_ISDIR(new_dentry->d_inode->i_mode);
+
+	if (old_dir != new_dir && old_is_dir != new_is_dir) {
+		if (old_is_dir) {
+			drop_nlink(old_dir);
+			inc_nlink(new_dir);
+		} else {
+			drop_nlink(new_dir);
+			inc_nlink(old_dir);
+		}
+	}
+	old_dir->i_ctime = old_dir->i_mtime =
+	new_dir->i_ctime = new_dir->i_mtime =
+	old_dentry->d_inode->i_ctime =
+	new_dentry->d_inode->i_ctime = CURRENT_TIME;
+
+	return 0;
+}
+
 static int shmem_rename2(struct inode *old_dir, struct dentry *old_dentry, struct inode *new_dir, struct dentry *new_dentry, unsigned int flags)
 {
-	if (flags & ~(RENAME_NOREPLACE))
+	if (flags & ~(RENAME_NOREPLACE | RENAME_EXCHANGE))
 		return -EINVAL;
 
-	return shmem_rename(old_dir, old_dentry, new_dir, new_dentry);
+	if (flags & RENAME_EXCHANGE)
+		return shmem_exchange(old_dir, old_dentry, new_dir, new_dentry);
+	else
+		return shmem_rename(old_dir, old_dentry, new_dir, new_dentry);
 }
 
 static int shmem_symlink(struct inode *dir, struct dentry *dentry, const char *symname)
