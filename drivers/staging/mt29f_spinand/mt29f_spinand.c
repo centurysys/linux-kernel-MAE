cc5d0dd495e4872304875bb92be35fef724f4cdf
Anusha Canchi Ramachandra Rao <anusha@codeaurora.org>
2015-10-09 14:22:47 +0530

    ipq40xx: spi nand: Fix Kw warnings
    Change-Id: I7489e3d2a49e99c76e1af9ccbb1257b8d6f83d63
    Signed-off-by: Anusha Canchi Ramachandra Rao <anusha@codeaurora.org>
    (cherry picked from commit 103f56474b6fa3119fd0f3d59468f94a3bf7221d)

--- drivers/staging/mt29f_spinand/mt29f_spinand.c
+++ drivers/staging/mt29f_spinand/mt29f_spinand.c
@@ -527,7 +527,7 @@ static int spinand_read_from_cache(struct spi_device *spi_nand, u16 page_id,
 static int spinand_read_page(struct spi_device *spi_nand, u16 page_id,
 			     u16 offset, u16 len, u8 *rbuf)
 {
-	int ret, ecc_error, ecc_corrected;
+	int ret, ecc_error = 0, ecc_corrected = 0;
 	u8 status = 0;
 	struct spinand_ops *dev_ops = get_dev_ops(spi_nand);
 	struct mtd_info *mtd = (struct mtd_info *)
@@ -669,6 +669,9 @@ static int spinand_program_page(struct spi_device *spi_nand,
 
 	enable_read_hw_ecc = 0;
 	wbuf = devm_kzalloc(&spi_nand->dev, CACHE_BUF, GFP_KERNEL);
+	if (!wbuf)
+		return -ENOMEM;
+
 	spinand_read_page(spi_nand, page_id, 0, CACHE_BUF, wbuf);
 
 	for (i = offset, j = 0; i < len; i++, j++)
@@ -831,7 +834,7 @@ static int spinand_read_page_hwecc(struct mtd_info *mtd, struct nand_chip *chip,
 				   u8 *buf, int oob_required, int page)
 {
 	int retval;
-	u8 status;
+	u8 status = 0;
 	u8 *p = buf;
 	int eccsize = chip->ecc.size;
 	int eccsteps = chip->ecc.steps;
@@ -889,7 +892,7 @@ static int spinand_wait(struct mtd_info *mtd, struct nand_chip *chip)
 
 	unsigned long timeo = jiffies;
 	int retval, state = chip->state;
-	u8 status;
+	u8 status = 0;
 
 	if (state == FL_ERASING)
 		timeo += (HZ * 400) / 1000;
