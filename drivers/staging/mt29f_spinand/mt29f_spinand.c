50f9f62beff0bc6e390ca163ac3bdd364527ecb6
Anusha Canchi Ramachandra Rao <anusha@codeaurora.org>
2015-10-23 09:16:27 +0530

    ipq40xx: spi nand: Fix mem alloc issue
    Change-Id: If82f8612fb7cc80bfd8ffcd6f319422f9e1ae1ee
    Signed-off-by: Anusha Canchi Ramachandra Rao <anusha@codeaurora.org>
    (cherry picked from commit a54d8ed976cf20e98d15cacfb9207de31b8f1b32)

--- drivers/staging/mt29f_spinand/mt29f_spinand.c
+++ drivers/staging/mt29f_spinand/mt29f_spinand.c
@@ -661,14 +661,14 @@ static int spinand_program_execute(struct spi_device *spi_nand, u16 page_id)
 static int spinand_program_page(struct spi_device *spi_nand,
 				u16 page_id, u16 offset, u16 len, u8 *buf)
 {
-	int retval;
+	int retval = 0;
 	u8 status = 0;
 	u8 *wbuf;
 #ifdef CONFIG_MTD_SPINAND_ONDIEECC
 	unsigned int i, j;
 
 	enable_read_hw_ecc = 0;
-	wbuf = devm_kzalloc(&spi_nand->dev, CACHE_BUF, GFP_KERNEL);
+	wbuf = kzalloc(CACHE_BUF, GFP_KERNEL);
 	if (!wbuf)
 		return -ENOMEM;
 
@@ -681,7 +681,7 @@ static int spinand_program_page(struct spi_device *spi_nand,
 		retval = spinand_enable_ecc(spi_nand);
 		if (retval < 0) {
 			dev_err(&spi_nand->dev, "enable ecc failed!!\n");
-			return retval;
+			goto exit;
 		}
 	}
 #else
@@ -690,7 +690,7 @@ static int spinand_program_page(struct spi_device *spi_nand,
 	retval = spinand_write_enable(spi_nand);
 	if (retval < 0) {
 		dev_err(&spi_nand->dev, "write enable failed!!\n");
-		return retval;
+		goto exit;
 	}
 	if (wait_till_ready(spi_nand))
 		dev_err(&spi_nand->dev, "wait timedout!!!\n");
@@ -698,23 +698,26 @@ static int spinand_program_page(struct spi_device *spi_nand,
 	retval = spinand_program_data_to_cache(spi_nand, page_id,
 					       offset, len, wbuf);
 	if (retval < 0)
-		return retval;
+		goto exit;
+
 	retval = spinand_program_execute(spi_nand, page_id);
 	if (retval < 0)
-		return retval;
+		goto exit;
+
 	while (1) {
 		retval = spinand_read_status(spi_nand, &status);
 		if (retval < 0) {
 			dev_err(&spi_nand->dev,
 				"error %d reading status register\n", retval);
-			return retval;
+			goto exit;
 		}
 
 		if ((status & STATUS_OIP_MASK) == STATUS_READY) {
 			if ((status & STATUS_P_FAIL_MASK) == STATUS_P_FAIL) {
 				dev_err(&spi_nand->dev,
 					"program error, page %d\n", page_id);
-				return -1;
+				retval = -1;
+				goto exit;
 			}
 			break;
 		}
@@ -724,7 +727,7 @@ static int spinand_program_page(struct spi_device *spi_nand,
 		retval = spinand_disable_ecc(spi_nand);
 		if (retval < 0) {
 			dev_err(&spi_nand->dev, "disable ecc failed!!\n");
-			return retval;
+			goto exit;
 		}
 		enable_hw_ecc = 0;
 	}
@@ -732,12 +735,16 @@ static int spinand_program_page(struct spi_device *spi_nand,
 	retval = spinand_write_disable(spi_nand);
 	if (retval < 0) {
 		dev_err(&spi_nand->dev, "write disable failed!!\n");
-		return retval;
+		goto exit;
 	}
 	if (wait_till_ready(spi_nand))
 		dev_err(&spi_nand->dev, "wait timedout!!!\n");
 
-	return 0;
+exit:
+#ifdef CONFIG_MTD_SPINAND_ONDIEECC
+	kfree(wbuf);
+#endif
+	return retval;
 }
 
 /**
