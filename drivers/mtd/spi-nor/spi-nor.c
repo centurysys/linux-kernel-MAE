6aebbbf793988cd2c1b01e5168bb3bc9fce0c0e3
Ram Chandra Jangir <rjangi@codeaurora.org>
2016-01-12 16:05:48 +0530

    OpenWRT: 141-Revert-mtd-spi-nor-disable-protection-for-Winbond-fl.patch
    Change-Id: I61dfd61bbaf5e8aaf2f5bbac490bc1b60ce59599
    Signed-off-by: Ram Chandra Jangir <rjangi@codeaurora.org>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -1194,14 +1194,13 @@ int spi_nor_scan(struct spi_nor *nor, const char *name, enum read_mode mode)
 	mutex_init(&nor->lock);
 
 	/*
-	 * Atmel, SST, Intel/Numonyx, and others serial NOR tend to power up
-	 * with the software protection bits set
+	 * Atmel, SST and Intel/Numonyx serial nor tend to power
+	 * up with the software protection bits set
 	 */
 
 	if (JEDEC_MFR(info) == SNOR_MFR_ATMEL ||
 	    JEDEC_MFR(info) == SNOR_MFR_INTEL ||
-	    JEDEC_MFR(info) == SNOR_MFR_SST ||
-	    JEDEC_MFR(info) == SNOR_MFR_WINBOND) {
+	    JEDEC_MFR(info) == SNOR_MFR_SST) {
 		write_enable(nor);
 		write_sr(nor, 0);
 	}
