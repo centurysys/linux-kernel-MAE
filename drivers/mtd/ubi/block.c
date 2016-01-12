7d561bdfc5905417b2efdd0a7164fc616666ab22
Ram Chandra Jangir <rjangi@codeaurora.org>
2016-01-12 16:06:19 +0530

    OpenWRT: 493-ubi-set-ROOT_DEV-to-ubiblock-rootfs-if-unset.patch
    Change-Id: Iff9c8494cd4be3db6ea4fdfd8ced21aeb14abf2d
    Signed-off-by: Ram Chandra Jangir <rjangi@codeaurora.org>

--- drivers/mtd/ubi/block.c
+++ drivers/mtd/ubi/block.c
@@ -50,6 +50,7 @@
 #include <linux/scatterlist.h>
 #include <linux/idr.h>
 #include <asm/div64.h>
+#include <linux/root_dev.h>
 
 #include "ubi-media.h"
 #include "ubi.h"
@@ -448,6 +449,15 @@ int ubiblock_create(struct ubi_volume_info *vi)
 	add_disk(dev->gd);
 	dev_info(disk_to_dev(dev->gd), "created from ubi%d:%d(%s)",
 		 dev->ubi_num, dev->vol_id, vi->name);
+
+	if (!strcmp(vi->name, "rootfs") &&
+	    config_enabled(CONFIG_MTD_ROOTFS_ROOT_DEV) &&
+	    ROOT_DEV == 0) {
+		pr_notice("ubiblock: device ubiblock%d_%d (%s) set to be root filesystem\n",
+			  dev->ubi_num, dev->vol_id, vi->name);
+		ROOT_DEV = MKDEV(gd->major, gd->first_minor);
+	}
+
 	return 0;
 
 out_free_queue:
