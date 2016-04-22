671c21145d576880c6aed27e2c82dacd45c7794e
Viresh Kumar <viresh.kumar@linaro.org>
2016-04-22 16:58:39 +0530

    cpufreq: dt: Mark platdev machines array as __initconst
    The machines array in cpufreq-dt-platdev is used only once at boot time
    and so should be marked with __initconst, so that kernel can free up
    memory used for it, if required.
    
    Suggested-by: Geert Uytterhoeven <geert@linux-m68k.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    (cherry picked from commit e92bb16674c5156ea9230ba7a3ab6d490750888f)
    
    Change-Id: I9ff278ad8a4839ac9736111e7c6f696e22b6d4fc
    Signed-off-by: Rajith Cherian <rajith@codeaurora.org>

--- drivers/cpufreq/cpufreq-dt-platdev.c
+++ drivers/cpufreq/cpufreq-dt-platdev.c
@@ -11,7 +11,7 @@
 #include <linux/of.h>
 #include <linux/platform_device.h>
 
-static const struct of_device_id machines[] = {
+static const struct of_device_id machines[] __initconst = {
 };
 
 static int __init cpufreq_dt_platdev_init(void)
