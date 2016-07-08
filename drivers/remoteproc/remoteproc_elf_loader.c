f40f51f4aa6a02e8dace2a585673e2927190f158
Manoharan Vijaya Raghavan <mraghava@codeaurora.org>
2016-07-08 14:31:05 +0530

    rproc: ipq807x: avoid loading segments with memsz as 0
    In wcss ELF we have some loadable segments for which
    both filesz and memsz is 0, avoid processing those segments.
    
    Change-Id: I84ed9c014cea2874c1af7939cf73e7b6d28279b6
    Signed-off-by: Manoharan Vijaya Raghavan <mraghava@codeaurora.org>

--- drivers/remoteproc/remoteproc_elf_loader.c
+++ drivers/remoteproc/remoteproc_elf_loader.c
@@ -162,7 +162,7 @@ rproc_elf_load_segments(struct rproc *rproc, const struct firmware *fw)
 		u32 offset = phdr->p_offset;
 		void *ptr;
 
-		if (phdr->p_type != PT_LOAD)
+		if ((phdr->p_type != PT_LOAD) || (memsz == 0))
 			continue;
 
 		dev_dbg(dev, "phdr: type %d da 0x%x memsz 0x%x filesz 0x%x\n",
