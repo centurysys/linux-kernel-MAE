e087da1b5bf8c87c76e7bea8abc17edae3bf0b49
Manoharan Vijaya Raghavan <mraghava@codeaurora.org>
2016-11-14 17:00:17 +0530

    ipq807x: wcss: Fix License and ownership
    Change-Id: If309ea442361838be0ab13cec4b9fb70b1ddad47
    Signed-off-by: Manoharan Vijaya Raghavan <mraghava@codeaurora.org>

--- drivers/remoteproc/q6v5-wcss.c
+++ drivers/remoteproc/q6v5-wcss.c
@@ -1,13 +1,16 @@
 /* Copyright (c) 2016 The Linux Foundation. All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include <linux/kernel.h>
@@ -356,7 +359,7 @@ static int q6_rproc_probe(struct platform_device *pdev)
 	q6v5_rproc_pdata = rproc->priv;
 	q6v5_rproc_pdata->rproc = rproc;
 	q6v5_rproc_pdata->emulation = of_property_read_bool(pdev->dev.of_node,
-					"qcom,emulation");
+					"qca,emulation");
 	rproc->has_iommu = false;
 
 	q6_fw_ops = *(rproc->fw_ops);
@@ -437,7 +440,7 @@ static int q6_rproc_remove(struct platform_device *pdev)
 }
 
 static const struct of_device_id q6_match_table[] = {
-	{ .compatible = "qcom,q6v5-wcss-rproc" },
+	{ .compatible = "qca,q6v5-wcss-rproc" },
 	{}
 };
 
@@ -454,4 +457,4 @@ static struct platform_driver q6_rproc_driver = {
 module_platform_driver(q6_rproc_driver);
 
 MODULE_LICENSE("GPL v2");
-MODULE_DESCRIPTION("QCOM Remote Processor control driver");
+MODULE_DESCRIPTION("QCA Remote Processor control driver");
