f235c5df6155ce8a599eb1531cea022acd1fa0b9
Nicolas Ferre <nicolas.ferre@microchip.com>
2017-06-12 15:43:45 +0200

    atmel_mxt_ts.c: move to current github.com/atmel-maxtouch
    This is driver from commit ID: 72c03333f35f7eddd707680d8e5de3527b5ef674.
    
    Signed-off-by: Nicolas Ferre <nicolas.ferre@microchip.com>
    [eugen.hristev@microchip.com: adapted original patch to 4.14]
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>
    [claudiu.beznea@microchip.com: adapt to 4.19, 5.4, 5.10, 5.15]
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- include/linux/platform_data/atmel_mxt_ts.h
+++ include/linux/platform_data/atmel_mxt_ts.h
@@ -22,7 +22,11 @@ struct mxt_platform_data {
 	u8 t19_num_keys;
 	const unsigned int *t19_keymap;
 	enum mxt_suspend_mode suspend_mode;
+	int t15_num_keys;
+	const unsigned int *t15_keymap;
 	unsigned long gpio_reset;
+	const char *cfg_name;
+	const char *input_name;
 };
 
 #endif /* __LINUX_PLATFORM_DATA_ATMEL_MXT_TS_H */
