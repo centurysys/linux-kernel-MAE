eebcd3c0056cc753cf3964324b3b86ac86b186c8
KASHIWAKURA Takashi <kashiwakura@centurysys.co.jp>
2018-11-16 17:34:09 +0900

    NXR: Change features on mk_uImage.sh
    (cherry picked from commit 0ea30e3de4c89c38c0cd6e26ac6982681fadb8d1)
    
     Conflicts:
    	README.pinecone

--- mk_uImage.sh
+++ mk_uImage.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-mkimage -A arm64 -O linux -C gzip -T kernel -a 0x80080000 -e 0x80080000 -n LS1046ARDB -d arch/arm64/boot/Image.gz uImage
+TARGET=$1
+
+usage()
+{
+    echo "Usage: $0 [LS1012ARDB|LS1012AFRDM|LS1012AFRWY|LS1046ARDB|ACORN|PINECONE]" >&2
+    exit 1
+}
+
+case $TARGET in
+    LS1012ARDB | LS1012AFRDM | LS1012AFRWY | LS1046ARDB | ACORN | PINECONE)
+        mkimage -A arm64 -O linux -C gzip -T kernel -a 0x80080000 -e 0x80080000 -n $TARGET -d arch/arm64/boot/Image.gz uImage
+        ;;
+    *)
+        usage
+        ;;
+esac
 
 exit $?
