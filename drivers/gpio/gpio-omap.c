ef2ef53e611504e85d0467b625304d024161b072
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2018-12-13 11:36:51 +0900

    drivers: gpio: gpio-omap: add support to use with wakeup sources.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- drivers/gpio/gpio-omap.c
+++ drivers/gpio/gpio-omap.c
@@ -1374,7 +1374,6 @@ static int omap_gpio_probe(struct platform_device *pdev)
 	irqc->irq_bus_lock = omap_gpio_irq_bus_lock,
 	irqc->irq_bus_sync_unlock = gpio_irq_bus_sync_unlock,
 	irqc->name = dev_name(&pdev->dev);
-	irqc->flags = IRQCHIP_MASK_ON_SUSPEND;
 	irqc->parent_device = dev;
 
 	bank->irq = platform_get_irq(pdev, 0);
