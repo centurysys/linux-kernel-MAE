c4965384625da0587c7492c7a647aca2c12214ba
Claudiu Beznea <claudiu.beznea@microchip.com>
2019-04-09 14:13:32 +0300

    usb: gadget: udc: atmel: add compatible for SAM9X60's PMC
    Add compatible for SAM9X60's PMC.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>
    [cristian.birsan@microchip.com Add sentinel at the end of the array]
    Signed-off-by: Cristian Birsan <cristian.birsan@microchip.com>

--- drivers/usb/gadget/udc/atmel_usba_udc.c
+++ drivers/usb/gadget/udc/atmel_usba_udc.c
@@ -2055,6 +2055,8 @@ static const struct of_device_id atmel_pmc_dt_ids[] = {
 	{ .compatible = "atmel,at91sam9g45-pmc" },
 	{ .compatible = "atmel,at91sam9rl-pmc" },
 	{ .compatible = "atmel,at91sam9x5-pmc" },
+	{ .compatible = "microchip,sam9x60-pmc" },
+	{ /* sentinel */ }
 };
 
 static struct usba_ep * atmel_udc_of_init(struct platform_device *pdev,
