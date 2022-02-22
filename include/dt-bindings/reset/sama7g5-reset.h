899b07464745289df9e1e7086e557b050b77973d
Claudiu Beznea <claudiu.beznea@microchip.com>
2022-02-22 11:20:49 +0200

    dt-bindings: reset: add sama7g5 support
    Add reset bindings for SAMA7G5. At the moment only USB PHYs are
    included.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

---
+++ include/dt-bindings/reset/sama7g5-reset.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __DT_BINDINGS_RESET_SAMA7G5_H
+#define __DT_BINDINGS_RESET_SAMA7G5_H
+
+#define RESET_USB_PHY1		4
+#define RESET_USB_PHY2		5
+#define RESET_USB_PHY3		6
+
+#endif /* __DT_BINDINGS_RESET_SAMA7G5_H */
