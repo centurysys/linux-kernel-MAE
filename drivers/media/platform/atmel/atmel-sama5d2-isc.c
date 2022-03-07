8d83298f2a6d8aba9536048c47b00fe3d0e1d195
Miaoqian Lin <linmq006@gmail.com>
2022-03-07 08:16:37 +0000

    media: atmel: atmel-isc: Fix PM disable depth imbalance in atmel_isc_probe
    The pm_runtime_enable will decrease power disable depth.
    If the probe fails, we should use pm_runtime_disable() to balance
    pm_runtime_enable().
    
    Fixes: 0a0e265 ("media: atmel: atmel-isc: split driver into driver base and isc")
    Signed-off-by: Miaoqian Lin <linmq006@gmail.com>
    [eugen.hristev@microchip.com: rebased on top of MC support patches]
    Signed-off-by: Eugen Hristev <eugen.hristev@microchip.com>

--- drivers/media/platform/atmel/atmel-sama5d2-isc.c
+++ drivers/media/platform/atmel/atmel-sama5d2-isc.c
@@ -570,7 +570,7 @@ static int atmel_isc_probe(struct platform_device *pdev)
 	ret = clk_prepare_enable(isc->ispck);
 	if (ret) {
 		dev_err(dev, "failed to enable ispck: %d\n", ret);
-		goto isc_probe_mc_init_err;
+		goto disable_pm;
 	}
 
 	/* ispck should be greater or equal to hclock */
@@ -586,6 +586,9 @@ static int atmel_isc_probe(struct platform_device *pdev)
 
 unprepare_clk:
 	clk_disable_unprepare(isc->ispck);
+
+disable_pm:
+	pm_runtime_disable(dev);
 
 isc_probe_mc_init_err:
 	isc_mc_cleanup(isc);
