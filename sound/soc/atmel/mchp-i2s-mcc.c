centurysys/linux-kernel-MAE

commit 2c1ab8c6360dbaa97821e3a8c4177ceabf17e94f

ASoC: atmel: mchp-i2s-mcc: calculate maxburst based on period size
The period size represents the size of the DMA descriptor. This
means that the period size can be used to compute a higher
maxburst value, to increase the performance of the DMA transfer.

Signed-off-by: Codrin Ciubotariu <codrin.ciubotariu@microchip.com>

sound/soc/atmel/mchp-i2s-mcc.c
@@ -504,12 +504,24 @@ static int mchp_i2s_mcc_is_running(struct mchp_i2s_mcc_dev *dev)
 	return !!(sr & (MCHP_I2SMCC_SR_TXEN | MCHP_I2SMCC_SR_RXEN));
 }
 
+static inline int mchp_i2s_mcc_period_to_maxburst(int period_size)
+{
+	if (!(period_size % 8))
+		return 8;
+	if (!(period_size % 4))
+		return 4;
+	if (!(period_size % 2))
+		return 2;
+	return 1;
+}
+
 static int mchp_i2s_mcc_hw_params(struct snd_pcm_substream *substream,
 				  struct snd_pcm_hw_params *params,
 				  struct snd_soc_dai *dai)
 {
 	unsigned long rate = 0;
 	struct mchp_i2s_mcc_dev *dev = snd_soc_dai_get_drvdata(dai);
+	int period_size = snd_pcm_lib_period_bytes(substream);
 	u32 mra = 0;
 	u32 mrb = 0;
 	unsigned int channels = params_channels(params);
@@ -630,11 +642,11 @@ static int mchp_i2s_mcc_hw_params(struct snd_pcm_substream *substream,
 	 * We must have the same burst size configured
 	 * in the DMA transfer and in out IP
 	 */
-	mrb |= MCHP_I2SMCC_MRB_DMACHUNK(channels);
+	mrb |= MCHP_I2SMCC_MRB_DMACHUNK(mchp_i2s_mcc_period_to_maxburst(period_size));
 	if (is_playback)
-		dev->playback.maxburst = 1 << (fls(channels) - 1);
+		dev->playback.maxburst = mchp_i2s_mcc_period_to_maxburst(period_size);
 	else
-		dev->capture.maxburst = 1 << (fls(channels) - 1);
+		dev->capture.maxburst = mchp_i2s_mcc_period_to_maxburst(period_size);
 
 	switch (params_format(params)) {
 	case SNDRV_PCM_FORMAT_S8:

commit 5bfd7f454cef7bddaecd70861ae5c320b420d7cf

ASoC: atmel: mchp-i2s-mcc: use sample size when calculating maxburst
The period size must be divided by sample size * maxburst, not just by
maxburst. This assures that all the DMA descriptors start from a well
aligned address.

Fixes: 2c1ab8c6360d ("ASoC: atmel: mchp-i2s-mcc: calculate maxburst based on period size")
Signed-off-by: Codrin Ciubotariu <codrin.ciubotariu@microchip.com>

sound/soc/atmel/mchp-i2s-mcc.c
@@ -504,13 +504,13 @@ static int mchp_i2s_mcc_is_running(struct mchp_i2s_mcc_dev *dev)
 	return !!(sr & (MCHP_I2SMCC_SR_TXEN | MCHP_I2SMCC_SR_RXEN));
 }
 
-static inline int mchp_i2s_mcc_period_to_maxburst(int period_size)
+static inline int mchp_i2s_mcc_period_to_maxburst(int period_size, int sample_size)
 {
-	if (!(period_size % 8))
+	if (!(period_size % (sample_size * 8)))
 		return 8;
-	if (!(period_size % 4))
+	if (!(period_size % (sample_size * 4)))
 		return 4;
-	if (!(period_size % 2))
+	if (!(period_size % (sample_size * 2)))
 		return 2;
 	return 1;
 }
@@ -521,7 +521,9 @@ static int mchp_i2s_mcc_hw_params(struct snd_pcm_substream *substream,
 {
 	unsigned long rate = 0;
 	struct mchp_i2s_mcc_dev *dev = snd_soc_dai_get_drvdata(dai);
+	int sample_bytes = params_physical_width(params) / 8;
 	int period_size = snd_pcm_lib_period_bytes(substream);
+	int maxburst;
 	u32 mra = 0;
 	u32 mrb = 0;
 	unsigned int channels = params_channels(params);
@@ -642,11 +644,12 @@ static int mchp_i2s_mcc_hw_params(struct snd_pcm_substream *substream,
 	 * We must have the same burst size configured
 	 * in the DMA transfer and in out IP
 	 */
-	mrb |= MCHP_I2SMCC_MRB_DMACHUNK(mchp_i2s_mcc_period_to_maxburst(period_size));
+	maxburst = mchp_i2s_mcc_period_to_maxburst(period_size, sample_bytes);
+	mrb |= MCHP_I2SMCC_MRB_DMACHUNK(maxburst);
 	if (is_playback)
-		dev->playback.maxburst = mchp_i2s_mcc_period_to_maxburst(period_size);
+		dev->playback.maxburst = maxburst;
 	else
-		dev->capture.maxburst = mchp_i2s_mcc_period_to_maxburst(period_size);
+		dev->capture.maxburst = maxburst;
 
 	switch (params_format(params)) {
 	case SNDRV_PCM_FORMAT_S8: