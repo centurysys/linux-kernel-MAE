199bf8f2765ade1dd756373d1cb44b90c87b49af
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2022-07-27 13:10:30 +0900

    drivers: net: ethernet: intel: igb: register netdev with "en%d" to swap "eth0" <-> "eth1" with udev.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>

--- drivers/net/ethernet/intel/igb/igb_main.c
+++ drivers/net/ethernet/intel/igb/igb_main.c
@@ -3485,7 +3485,7 @@ static int igb_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 	 */
 	igb_get_hw_control(adapter);
 
-	strcpy(netdev->name, "eth%d");
+	strcpy(netdev->name, "en%d");
 	err = register_netdev(netdev);
 	if (err)
 		goto err_register;
