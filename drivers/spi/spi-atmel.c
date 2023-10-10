e0e854e174feecbef4a2f4a6472f442ded5bc025
Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
2023-10-10 09:33:36 +0900

    drivers: spi: spi-atmel: add support for MorseMicro's WiFi HaLow driver.
    Signed-off-by: Takeyoshi Kikuchi <kikuchi@centurysys.co.jp>
    (cherry picked from commit ea88cc9ee268d0dd995a5c4751d9915fd43d2bca)

--- drivers/spi/spi-atmel.c
+++ drivers/spi/spi-atmel.c
@@ -1255,6 +1255,9 @@ static int atmel_spi_setup(struct spi_device *spi)
 
 	as = spi_controller_get_devdata(spi->controller);
 
+	if (spi->mode & SPI_NO_CS)
+		return 0;
+
 	/* see notes above re chipselect */
 	if (!spi_get_csgpiod(spi, 0) && (spi->mode & SPI_CS_HIGH)) {
 		dev_warn(&spi->dev, "setup: non GPIO CS can't be active-high\n");
@@ -1322,6 +1325,12 @@ static int atmel_spi_setup(struct spi_device *spi)
 static void atmel_spi_set_cs(struct spi_device *spi, bool enable)
 {
 	struct atmel_spi *as = spi_controller_get_devdata(spi->controller);
+
+	if (spi->mode & SPI_NO_CS) {
+		cs_deactivate(as, spi);
+		return;
+	}
+
 	/* the core doesn't really pass us enable/disable, but CS HIGH vs CS LOW
 	 * since we already have routines for activate/deactivate translate
 	 * high/low to active/inactive
@@ -1333,7 +1342,6 @@ static void atmel_spi_set_cs(struct spi_device *spi, bool enable)
 	} else {
 		cs_deactivate(as, spi);
 	}
-
 }
 
 static int atmel_spi_one_transfer(struct spi_controller *host,
